9977d295e9e2b4b9160a7d5f673b5af4216bf664
zibrulyaa <pzibrov@icloud.com>
2020-05-08 20:46:00 +0300

    added animation

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,7 @@ gulp.task("style", function () {
       "node_modules/slick-carousel/slick/slick.css",
       "node_modules/normalize.css/normalize.css",
       "node_modules/magnific-popup/dist/magnific-popup.css",
+      "node_modules/animate.css/animate.css",
     ])
     .pipe(concat("libs.min.css"))
     .pipe(cssmin())
@@ -37,6 +38,7 @@ gulp.task("script", function () {
     .src([
       "node_modules/slick-carousel/slick/slick.js",
       "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
+      "node_modules/wow.js/dist/wow.js",
     ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
