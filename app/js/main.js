6c655160c769f08c2909e140d846632a7184039b
zibrulyaa <pzibrov@icloud.com>
2020-05-10 14:51:05 +0300

    adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
   new WOW().init();
+  if ($(window).width() < 992) {
+    var wow = new WOW({
+      mobile: false,
+    });
+    new WOW().init();
+  }
+  $(".header__btn").on("click", function () {
+    $(".menu__list").slideToggle(".active");
+  });
 });
